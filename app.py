481bbc2e198ada6930a053fece6ccad856b664c1
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-11-06 11:46:10 +0200

    feat: Convert to AJAX

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:n1k1kn0x@localhost:5433/todoapp'
@@ -25,11 +26,33 @@ def index():
 
 
 @app.route('/todos/create', methods=['POST'])
-def create():
-    todo = request.form.get('description')
-    db.session.add(Todo(description=todo))
-    db.session.commit()
-    return redirect(url_for('index'))
+def create_todo():
+    error = False
+    body = {}
+    try:
+        # todo = request.form.get('description')
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+        # return redirect(url_for('index'))
+        # 'index' is the name of the controller for route '/'
+    except():
+        db.session.rollback()
+        error = True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify(body)
+    # if not error:
+    #     # return jsonify({
+    #     #     'description': todo.description
+    #     # })
+    #     return jsonify(body)
 
 
 if __name__ == '__main__':
